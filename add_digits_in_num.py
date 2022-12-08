3942f44b7879a8648dd716375f9abb8927bc1b7a
Aaron Then <aaronafter@hotmail.com>
2022-12-08 15:51:00 +0800

    Adding add_digits_in_num file

---
+++ add_digits_in_num.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+total = int(two_digit_number[0]) + int(two_digit_number[1])
+print (total)
+
+# first_digit = two_digit_number [0]
+# second_digit = two_digit_number [1]
+# results = int(first_digit) + int (second_digit)
+# print (results)
